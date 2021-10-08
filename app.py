a42387f2d8c65cd32df2f72d3e845fcd54b13a07
Álef Vieira Coutinho <alef_coutin@hotmail.com>
2021-10-08 19:59:14 -0300

    atualizaei o css

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def pag_graficos():
 
 
 if __name__ == "__main__":
-    app.run()
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)
+    # app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
 
